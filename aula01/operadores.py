5e86278eaec6d81a539ba3b1764cc659e64d9ae4
Fernando <fernandotmgomes@gmail.com>
2025-08-07 10:49:48 -0300

    operadores

---
+++ aula01/operadores.py
@@ -0,0 +1,46 @@
+#variaveis
+n1 = 10
+n2 = 10
+
+#"titulo" na tela
+print(40*"-","Operadores",40*"-")
+
+#operadores
+soma = n1+n2
+divi = n1/n2
+mult = n1*n2
+expo = n1 ** n2
+subt = n1-n2
+divi_int = n1//n2
+resto = n1 %2
+
+#mostrar as operações na tela
+print("Resultado da soma:",n1,"+",n2,"=",soma) #soma
+print("Resultado da divisão:",n1,"/",n2,"=",divi) #divisão
+print("Resultado da multiplicação:",n1,"*",n2,"=",mult) #multiplicação
+print("Resultado do expoente:",n1,"**",n2,"=",expo) #exponencial
+print("Resultado da subtração:",n1,"-",n2,"=",subt) #subtração
+print("Resultado da divisão inteira:",n1,"//",n2,"=",divi_int) #divisão inteira
+print("Resultado do resto de :",n1,"=",resto) #resto da divisão por dois
+
+print(40*"-","Operadores de comparação",40*"-")
+
+#operadores de coparação
+n1 > n2
+n1 < n2
+n1 == n2
+n1 >= n2
+n1 <= n2
+n1 != n2
+
+#operadores de atribuição
+ano = 2025 #variavel
+acre = 1 #variavel de acrescimo de ano
+decre = 1 #variavel de decrescimo do ano
+print("Ano atual:",ano)
+ano += acre #atribuição
+print("Depois de",acre,"anos, estaremos em:",ano)
+ano -= acre #atribuição
+ano -= decre #atribuição
+print("Se voltamos no tempo a",decre,"anos, estaremos em:",ano)
+ano -= decre #atribuição
