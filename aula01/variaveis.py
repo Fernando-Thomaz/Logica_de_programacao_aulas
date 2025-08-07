f19a27b17468a05ae0c1b6ab3e4270e274743503
Fernando <fernandotmgomes@gmail.com>
2025-08-07 09:31:04 -0300

    meu primeiro script

---
+++ aula01/variaveis.py
@@ -0,0 +1,10 @@
+#variaveis
+nome = "Fernando"
+idade = 16
+dt = "12 de agosto de 2008"
+
+#"titulo" na tela
+print(40*"-","Olá mundo esse é meu primeiro script em python",40*"-")
+
+#informações da pessoa aparece na tela
+print("Olá meu nome é:",nome,"\nTenho",idade,"anos","\nNasci em",dt)
