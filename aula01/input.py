695322087b3ed8cf992e58e536076e8636a9f079
Fernando <fernandotmgomes@gmail.com>
2025-08-07 11:12:31 -0300

    entrada de dados

--- aula01/input.py
+++ aula01/input.py
@@ -1,4 +1,10 @@
+#"titulo na tela"
 print(40*"-","Entrada de dados",40*"-")
 
+#codigo que necessita de entrada de dados "nome"
 nome_usuario = input("Digite o seu nome: ")
 print("Seja bem-vindo ao seu sistema python",nome_usuario)
+
+#"titulo"
+print(40*"-","Calculadora",40*"-")
+
