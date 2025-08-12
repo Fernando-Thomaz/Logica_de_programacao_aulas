19e0ded33f8b833faca964b2b4ec18508ee31974
Fernando <fernandotmgomes@gmail.com>
2025-08-12 08:07:21 -0300

    convertendo dados de um tipo para outro

--- aula01/converter_dados.py
+++ aula01/converter_dados.py
@@ -5,6 +5,6 @@ print(30*"-","Convertendo dados",30*"-")
 ano = input("Digite seu ano de nascimento: ")
 print(type(ano))
 
-#convertendo dados para ineteiro
+#convertendo dados de string para inteiro
 ano = int(ano)
 print(type(ano))
