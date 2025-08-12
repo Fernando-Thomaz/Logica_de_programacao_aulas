effd4419470d6c20a67a85699acf15f1b8f0dcb5
Fernando <fernandotmgomes@gmail.com>
2025-08-12 09:22:31 -0300

    atividade 4

---
+++ aula02/atividade.py
@@ -0,0 +1,14 @@
+#dados
+n1 = "20"
+n2 = 20
+
+#mostrando o tipo de dado da variavel n1
+print(f"O tipo de variavel n1 é {type(n1)}")
+
+#transformando o tipo de dado de string para inteiro
+n1 = int(n1)
+print(f"O tipo de variavel n1 é {type(n1)}")
+
+#somando os dois
+soma = n1+n2
+print(f"A soma: {n1} + {n2} = {soma}")
