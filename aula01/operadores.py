cd1f8bc8ad94fc7e10fc917898ad0f0251395a1d
Fernando <fernandotmgomes@gmail.com>
2025-08-12 08:08:35 -0300

    operadores logicos

--- aula01/operadores.py
+++ aula01/operadores.py
@@ -23,24 +23,31 @@ print("Resultado da subtração:",n1,"-",n2,"=",subt) #subtração
 print("Resultado da divisão inteira:",n1,"//",n2,"=",divi_int) #divisão inteira
 print("Resultado do resto de :",n1,"=",resto) #resto da divisão por dois
 
+#"titulo"
 print(40*"-","Operadores de atribuição",40*"-")
 
 #operadores de coparação
-n1 > n2
-n1 < n2
-n1 == n2
-n1 >= n2
-n1 <= n2
-n1 != n2
+n1 > n2 #maior que
+n1 < n2 #menor que
+n1 == n2 #iguais
+n1 >= n2 #maior ou igual
+n1 <= n2 #menor ou igual
+n1 != n2 #diferente que
 
 #operadores de atribuição
 ano = 2025 #variavel
 acre = 1 #variavel de acrescimo de ano
 decre = 1 #variavel de decrescimo do ano
+
+#colocando o ano atual na tela
 print("Ano atual:",ano)
 ano += acre #atribuição
+
+#mostra o ano adicionando um
 print("Depois de",acre,"anos, estaremos em:",ano)
 ano -= acre #atribuição
 ano -= decre #atribuição
+
+#mostra o ano diminuindo um
 print("Se voltamos no tempo a",decre,"anos, estaremos em:",ano)
 ano -= decre #atribuição
