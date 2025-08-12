fc76a946df9a0c94f73bf6f2a8e607012c0ae637
Fernando <fernandotmgomes@gmail.com>
2025-08-12 11:01:18 -0300

    if else

--- aula02/if_else.py
+++ aula02/if_else.py
@@ -6,13 +6,16 @@ alterando virgula e ponto
 if else
 operadores ternario
 '''
-
-#dados
-nome = "Fernando"
-idade = 17
-altura = 1.74
+#entrada de dados
+nome = str(input(f"Qual é seu nome? "))
+idade = int(input(f"Quantos anos você tem? "))
+peso = float(input("Digite o seu peso: ").replace(",","."))#entrada de dados tratando o "," como um "."
+altura = float(input("Digite o seu altura: ").replace(",","."))#entrada de dados tratando o "," como um "." 
 
 #NOTE - concatenação com +
+
+#quebra de linha
+print(40*"-")
 
 #saida de dados
 print("Olá, meu nome é " + nome + ", tenho " + str(idade) + " anos de idade")
@@ -34,14 +37,23 @@ print(f"Olá, meu nome é {nome}, tenho {idade} anos de idade")
 
 #NOTE - eliminando quebra de linha
 
+#quebra de linha
+print(40*"-")
+
 #colocando duas linhas de codigo em uma linha de saida de dados
 print(f"O sábio sabia ", end="")
 print(f"que sabiá sabia assobiar.")
 
 #NOTE - limitando quantidade de casas depois da virgula
 
+#quebra de linha
+print(40*"-")
+
 #dados com muitos números depois da virgula
 valor = 1.23456789
+
+#exibindo o valor sem limitar casas
+print(f"{valor}")
 
 #exibindo apenas duas casas depois da virgula
 print(f"{valor:.2f}")
@@ -51,8 +63,18 @@ print(40*"-")
 
 #NOTE - tratar virgula como ponto
 
-#entrada de dados, trocando a virgula como ponto
-peso = float(input("Digite o seu peso: ").replace(",","."))
-
 #mostrando na tela
-print(f"Seu peso é {peso:.2f}".replace(".",","))
+print(f"Seu peso é {peso:.2f}".replace(".",","))#saida de dados tratando o "." como ","
+
+#NOTE - if else
+
+#quebra de linha
+print(40*"-")
+
+if idade >= 18:
+    print(f"{nome} é maior de idade.")
+    print(f"Você esta dentro do bloco do if.")
+
+else:
+    print(f"{nome} é menor de idade.")
+    print(f"Você esta dentro do bloco do else.")
